936b9ba183de5ecfa5bd74c94383aa4c78235a42
Ben Dang <s224116668@deakin.edu.au>
2024-09-11 13:15:30 +1000

    update

--- Project1/app/models/document_model.py
+++ Project1/app/models/document_model.py
@@ -1,31 +1,34 @@
-from app import db
 from bson.objectid import ObjectId
+from app import db 
+# Get the collection name from environment variables
+collection_name = "data"
 
 class DocumentModel:
     @staticmethod
     def get_all_documents():
-        collection = db['sales']  # Replace with your collection name
+        # Access the collection using the name from environment variables
+        collection = db[collection_name]
         return list(collection.find({}, {'_id': 0}))
 
     @staticmethod
     def get_document_by_id(document_id):
-        collection = db['sales']
+        collection = db[collection_name]
         return collection.find_one({"_id": ObjectId(document_id)}, {'_id': 0})
 
     @staticmethod
     def insert_document(data):
-        collection = db['sales']
+        collection = db[collection_name]
         inserted_id = collection.insert_one(data).inserted_id
         return str(inserted_id)
 
     @staticmethod
     def update_document(document_id, data):
-        collection = db['sales']
+        collection = db[collection_name]
         result = collection.update_one({"_id": ObjectId(document_id)}, {"$set": data})
         return result.modified_count
 
     @staticmethod
     def delete_document(document_id):
-        collection = db['sales']
+        collection = db[collection_name]
         result = collection.delete_one({"_id": ObjectId(document_id)})
         return result.deleted_count
